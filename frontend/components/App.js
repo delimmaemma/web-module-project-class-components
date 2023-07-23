6027d645031053f3b1fdd474010afb7bff0f7603
delimmaemma <calvinhtml@gmail.com>
2023-07-23 18:40:33 -0500

    builds from state and completed flag

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,40 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Scoop the kitties',
+          completed: false
+        },
+        {
+          id: 2,
+          text: 'Take out the trash',
+          completed: false
+        },
+        {
+          id: 3,
+          text: 'Put up the dishes',
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <h1>Todos</h1>
         <ul>
-          <li>Scoop the kitties</li>
-          <li>Take out the trash</li>
-          <li>Unload the dishwasher</li>
+          {
+            this.state.todos.map(todo => 
+              <li key={todo.id}>
+                {todo.text} {todo.completed && <span> - Completed!</span>}
+              </li>
+            ) 
+          }
         </ul>
         <form>
           <input/>
